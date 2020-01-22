ca5478cb256566694c3640660823b688ca8cac2f
kremir <kremir@o2.pl>
2020-01-22 19:26:23 +0100

    Initial Commit for Spring Boot Reddit Clone - educational purpose

---
+++ src/test/java/com/miro/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
