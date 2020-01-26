891c13100ea94499fd8015befd222f268ea01553
kremir <kremir@o2.pl>
2020-01-26 16:38:07 +0100

    Adding hibernate mappings for link->comments

--- src/main/java/com/miro/springit/domain/Link.java
+++ src/main/java/com/miro/springit/domain/Link.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +22,7 @@ public class Link {
     private String url;
 
     // comments
+    @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<>();
 
 }
