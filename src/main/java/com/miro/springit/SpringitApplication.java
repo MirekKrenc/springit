1eea6691ccd55576596e92eab8385c5a4f6f8344
kremir <kremir@o2.pl>
2020-02-04 11:22:39 +0100

    Setting up MySQL database

--- src/main/java/com/miro/springit/SpringitApplication.java
+++ src/main/java/com/miro/springit/SpringitApplication.java
@@ -1,6 +1,10 @@
 package com.miro.springit;
 
 import com.miro.springit.config.SpringitConfiguration;
+import com.miro.springit.domain.Comment;
+import com.miro.springit.domain.Link;
+import com.miro.springit.repository.CommentRepository;
+import com.miro.springit.repository.LinkRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +16,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableJpaAuditing
 public class SpringitApplication {
 
 	@Autowired
@@ -53,6 +59,27 @@ public class SpringitApplication {
 			log.info("CommandLineRunner.run();");
 			log.debug("CommandLineRunner.run();");
 			log.trace("CommandLineRunner.run();");
+		};
+	}
+
+	@Bean
+	CommandLineRunner databaseRunner(LinkRepository linkRepository, CommentRepository commentRepository)
+	{
+		return args -> {
+			Link link = new Link("Drogie borowki w sklepie", "https://sklep6028485.homesklep.pl/pl/p/Borowki/23");
+			linkRepository.save(link);
+
+			Comment comment = new Comment("Borowki sa drogie ale bardzo smaczne", link);
+			commentRepository.save(comment);
+
+			link.addComment(comment);
+
+			Comment comment1 = new Comment("Pychotki te borowki", link);
+			commentRepository.save(comment1);
+
+			link.addComment(comment1);
+
+			//System.out.println(link.getComments());
 		};
 	}
 
--- src/main/java/com/miro/springit/domain/Link.java
+++ src/main/java/com/miro/springit/domain/Link.java
@@ -1,12 +1,12 @@
 package com.miro.springit.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,11 +18,26 @@ public class Link extends Auditable{
     @Id
     @GeneratedValue
     private Long id;
+    @NonNull
     private String title;
+    @NonNull
     private String url;
+
 
     // comments
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<>();
 
+    public Link(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
 }
